f3c48d2a02b17bd0c658bd5217fa0ba65e706650
NicoleGL <nicole.glanga@gmail.com>
2024-01-18 22:01:58 +0100

    Holi

--- notas.c
+++ notas.c
@@ -27,3 +27,13 @@ typedef struct clase {
 t_clase *guardar_datos(FILE *f) {
 
 }
+
+/* Poner el nombre del archivo (notas.txt) como argumento */
+int main(int argc, char* argv[]) {
+    if(argc != 2) {
+        fprintf(stderr, "Argumentos incorrectos.");
+    }
+
+    FILE *f = fopen(argv[1], "r");
+    guardar_datos(f);
+}
